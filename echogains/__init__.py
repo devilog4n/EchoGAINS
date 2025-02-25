722ce8f78fccee3ed510e847c99c5b3b2fbbf0c5
Gilles Van De Vyver <gilles.van.de.vyver@ntnu.no>
2025-02-25 18:11:47 +0100

    Add initial prepare reaugment version

--- echogains/__init__.py
+++ echogains/__init__.py
@@ -1,4 +1,5 @@
 __version__ = '0.1.1'
 
 from echogains.downloads import download_data_sample
-
+from echogains.visualization import create_visualization
+from echogains.prepare_reaugment import prepare_gen_aug_seg,prepare_gen_aug_seg_sample
